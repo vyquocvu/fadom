83119bb528e693e877884de9096e74f3fc378477
Vũ. Vy Quốc <vyquocvu@gmail.com>
2023-02-19 14:21:10 +0700

    run test

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import createElement from '../dom/createElement';
+import render from '../dom/render';
+import mountElement from '../dom/mount';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const createVApp = (count: number) =>  createElement('div', {
+  attrs: {
+    id: 'app',
+  },
+  children: [
+    'The current count is: ' + count,
+    createElement('img', {
+      attrs: {
+        src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif',
+      },
+    }),
+  ],
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let count = 0;
+const $app = render(createVApp(count));
+let $rootEl = mountElement($app, document.getElementById('app'));
+
+setInterval(() => {
+  count++;
+  const updatedVApp = render(createVApp(count));
+  $rootEl = mountElement(updatedVApp, $rootEl);
+}, 10000);
