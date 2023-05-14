f79418ca57512c08aa408c4764cbfb5a7bb61614
Vy Quốc Vũ <vyquocvu@gmail.com>
2023-05-14 15:31:43 +0700

    update npm

---
+++ dom/index.ts
@@ -0,0 +1,11 @@
+import createElement from './createElement';
+import updateElement from './updateElement';
+import render from './render';
+import mountElement from './mount';
+
+export default {
+    render,
+    createElement,
+    updateElement,
+    mountElement
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
-import createElement from '../dom/createElement';
-import updateElement from '../dom/updateElement';
-import render from '../dom/render';
-import mountElement from '../dom/mount';
+import fadom from '../dom';
 
-const createVApp = (count: number) =>  createElement('div', {
+const createVApp = (count: number) =>  fadom.createElement('div', {
   attrs: {
     id: 'app',
   },
   children: [
     'The current count is: ' + count,
-    createElement('img', {
+    fadom.createElement('img', {
       attrs: {
         src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif',
       },
@@ -19,11 +16,11 @@ const createVApp = (count: number) =>  createElement('div', {
 
 let count = 0;
 const app = createVApp(count);
-const $app = render(app);
-const $rootEl = mountElement($app, document.getElementById('app'));
+const $app = fadom.render(app);
+const $rootEl = fadom.mountElement($app, document.getElementById('app'));
 
 setInterval(() => {
   count++;
   const updatedVApp = createVApp(count);
-  updateElement($rootEl as Element, updatedVApp, app);
+  fadom.updateElement($rootEl as Element, updatedVApp, app);
 }, 5000);
